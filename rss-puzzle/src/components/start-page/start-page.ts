b83046d6cfb57b8ff24f49645a745cf1790254df
olga <94057960+lustrochka@users.noreply.github.com>
2024-03-09 16:03:24 +0100

    feat: add personal greeting (#31)

--- rss-puzzle/src/components/start-page/start-page.ts
+++ rss-puzzle/src/components/start-page/start-page.ts
@@ -13,7 +13,8 @@ class StartPage extends Component {
         `RSS Puzzle is an interactive mini-game aimed at enhancing English language skills. 
         Players assemble sentences from jumbled words, inspired by Lingualeo's Phrase Constructor training. 
         The game integrates various levels of difficulty, hint options, and a unique puzzle-like experience with artwork.`
-      )
+      ),
+      p('start-page__greeting', `Hello, ${localStorage.getItem('name')} ${localStorage.getItem('surname')}!`)
     );
   }
 }
